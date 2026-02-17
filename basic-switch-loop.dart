fc142e59cdfbae47686ab540f1346736f7c1cb55
Atharva Gupta <atharvagupta00001@gmail.com>
2026-02-17 20:19:00 +0530

    Add files via upload

---
+++ basic-switch-loop.dart
@@ -0,0 +1,41 @@
+// void main() {
+//   String name = "Atharva";
+//   int age = 20;
+//   var namee = "Gupta";
+
+//   print("My name is $name");
+//   print("$namee");
+//   print("My age is $age");
+// }
+
+// void main(){
+// int  n = 10;
+// switch(n){
+//   case 1:
+//   print("I am good");
+//   break;
+//   case 2:
+//   print("I am not good");
+//   break;
+//   default:
+//   print("Confused");
+//   break;
+// }
+// }
+
+
+//loop
+// void main(){
+//   int n = 10;
+//   for (var i=0;i<n;i++){
+//     print(i);
+//   }
+// }
+
+//loop in aarrayy
+void main(){
+var n = [1,2,3,4,5,6,7];
+for (var i in n){
+  print(i);
+}
+}
