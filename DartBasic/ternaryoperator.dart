27e9f78626cd318e31e171bf7162949078553f9a
Atharva Gupta <atharvagupta00001@gmail.com>
2026-02-18 13:50:18 +0530

    Add files via upload

---
+++ DartBasic/ternaryoperator.dart
@@ -0,0 +1,16 @@
+// void main(){
+//   int num = 10;
+//   var result = num%2==0 ? 'Even' : 'Odd';
+//   print(result);
+// }
+
+//one more thing
+void main(){
+  var n = '10';
+  if (n is int){
+    print("integer");
+  }
+  else{
+    print(null);
+  }
+}
