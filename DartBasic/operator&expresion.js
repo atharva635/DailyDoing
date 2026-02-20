8aa8c8c6a022f1ca37794b020f57a5d7c35167f4
Atharva Gupta <atharvagupta00001@gmail.com>
2026-02-20 23:57:40 +0530

    Add files via upload

---
+++ DartBasic/operator&expresion.js
@@ -0,0 +1,20 @@
+//create an emp obj in js having 3 parameter empid, dept, salary , create a copy of this obj to employee
+//destructuring of array and object
+//spread
+//callback hell
+//promises
+//asynchronous
+//setTimeout
+//try catch
+function bakecake()
+{
+    console.log("1 cake in oven");
+    setTimeout(() =>{
+
+    console.log("2. cake in in");
+    mycallback();
+
+    },2000)
+    bakecake()
+}
+//write a program in js to check weather u go to kashmir for not according to availabilty of petrol
