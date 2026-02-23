58d20ee2e6c5dc0fc884c4663829dda7d88f4bad
Atharva Gupta <atharvagupta00001@gmail.com>
2026-02-23 16:10:59 +0530

    Add files via upload

---
+++ DartBasic/List.dart
@@ -0,0 +1,26 @@
+// void main(){
+//   List name = [1,2,3,4,5,6,'heloooo'];
+//   var nameeee = [1,2,3,4,'hello'];
+//   print(name[6]);
+//   print(nameeee[4]);
+// }
+
+
+// void main(){
+//   List <String> name = ['atharva','jack','nobita'];
+//   for(var i in name){
+//     print(i);
+//   }
+// }
+
+
+//spread operator
+//in this we see that the old value is seen
+// void  main(){
+//   List <String> name = ['hello','jack','lone','jickk','hgdhf'];
+//   var nameee = [...name];
+//   name[0] = 'atharva';
+//   for (var i in nameee){
+//     print(i);
+//   }
+// }
